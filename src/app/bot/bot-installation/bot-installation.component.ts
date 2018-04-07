0f3e250a6b179938cc1919fd5d2a85d06192a981
Shankar Morwal <shankar@habilelabs.io>
2018-04-07 16:44:17 +0530

    link ID issue done

--- src/app/bot/bot-installation/bot-installation.component.ts
+++ src/app/bot/bot-installation/bot-installation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SidebarService} from '../../shared/sidebar/sidebar.service';
 
 @Component({
   selector: 'app-bot-installation',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bot-installation.component.css']
 })
 export class botInstallationComponent implements OnInit {
-
-  constructor() { }
+    botData: any = {};
+  constructor(public sbs: SidebarService) { }
 
   ngOnInit() {
+      if (this.sbs.savedData) {
+          this.botData = this.sbs.savedData;
+      }
+      this.sbs.botList.subscribe((data) => {
+          this.botData = data[0];
+      });
+
+      this.sbs.botData.subscribe((data) => {
+          this.botData = data;
+      });
   }
 
 }
