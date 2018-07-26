d5063e178bb8daaf4c7ffa8facc896755ee12b20
harshit mehta <harshit@habilelabs.io>
2018-07-26 11:57:20 +0530

    my bot page ui changes

--- src/app/bot/bot-home/bot-home.component.ts
+++ src/app/bot/bot-home/bot-home.component.ts
@@ -15,7 +15,25 @@ export class BotHomeComponent implements OnInit, OnDestroy {
   data: any;
   botData: any = {};
   analytics_token: string;
+  showDiv = false;
+  previewDiv = false;
+  configureDiv = false;
+  lookDiv = false;
+  feedbackDiv = false;
+  insertDiv = false;
+  reportDiv = false;
+  installDiv = false;
   previewBot: string;
+  showBotConfig = false;
+  lookConfig = false;
+  previewConfig = false;
+  installConfig = false;
+  knowledgeConfig = false;
+  inviteConfig = false;
+  reportConfig = false;
+  productConfig = false;
+  showDelete = false;
+  showEditPass = false;
   constructor(public sbs: SidebarService, public botService: BotService, public snackBarService: SnackBarService) {}
 
   ngOnInit() {
@@ -41,11 +59,61 @@ export class BotHomeComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.sbs.savedData = this.botData;
   }
-
+  onFaqOutside(event) {
+    if (event) {
+      this.showDiv = false;
+    }
+  }
+  onPreviewOutside(event) {
+    if (event) {
+      this.previewDiv = false;
+    }
+  }
+  onInsertOutside(event) {
+    if (event) {
+      this.insertDiv = false;
+    }
+  }
+  onLookOutside(event) {
+    if (event) {
+      this.lookDiv = false;
+    }
+  }
+  onInstallOutside(event) {
+    if (event) {
+      this.installDiv = false;
+    }
+  }
+  onReportOutside(event) {
+  if (event) {
+    this.reportDiv = false;
+  }
+}
+  onConfigureOutside(event) {
+    if (event) {
+      this.configureDiv = false;
+    }
+  }
+  onFeedbackOutside(event) {
+    if (event) {
+      this.feedbackDiv = false;
+    }
+  }
   changeBotStatus() {
     const bot = {
       active: this.botData.active
     };
+    this.editBot(bot);
+  }
+
+  editBotConfig() {
+    const bot = {
+      token: this.botData.token
+    };
+    this.editBot(bot);
+  }
+
+  editBot(bot) {
     this.botService.editBot(bot, this.botData.id).subscribe((data) => {
       this.botData = data;
       this.snackBarService.openSnackBar('Bot Status Updated');
